9ad081ac86de9359ce0de562e5f22d89757d27ab
Leon <turkov.leon-2@yandex.ru>
2022-10-08 21:36:34 +0300

    Finished Ex 2

--- Ex_2.py
+++ Ex_2.py
@@ -1,8 +1,9 @@
 # Напишите программу для проверки истинности утверждения 
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+# not(x or y or z) == (not x) and (not y) and (not z)
 
-x = True
-y = True
-z = True
-result = not(x or y or z) == (not x) and (not y) and (not z)
-print(result)
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            print (f"Values x, y, z: {x} {y} {z}")
+            print(f"Result: {not(x or y or z) == (not x) and (not y) and (not z)}")
